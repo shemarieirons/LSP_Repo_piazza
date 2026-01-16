fcf29679b9fa3945c389f65dea2d9da7197f8cde
Shem <shem@Shemaries-MacBook-Pro.local>
2026-01-15 23:21:01 -0500

    First commit from java

--- src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,9 +1,9 @@
+/**
+ * Name: Shemarie Irons
+ */
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
-    /**
-     * Name: Shemarie Irons
-     */
     public static void main(String[] args) {
         System.out.println("Hello World!!");
     }
